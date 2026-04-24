c26dc2832d88bbbdf9dded46417e07a3e9348a9f
Deep-Rewale <deeprewale2004@gmail.com>
2026-04-24 13:39:40 +0530

    button fx

--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom'
 const Button = ({text, Redirect,paddingX,paddingY,textSize,hoverText}) => {
   return (
     <Link to={Redirect}>
-    <button className={`${paddingX} ${paddingY} ${textSize} border bg-accent font-semibold text-black  border-[#e8a020] rounded-md  cursor-pointer hover:border-[#e8a020] hover:bg-transparent hover:${hoverText}   transition-all duration-300 ease-in-out shadow-md origin-center hover:scale-105 hover:shadow-lg hover:shadow-[#e8a020]/40`}>
+    <button className={`${paddingX} ${paddingY} ${textSize} border bg-accent font-medium text-black  border-[#e8a020] rounded-md  cursor-pointer hover:border-[#e8a020] hover:bg-transparent hover:${hoverText}   transition-all duration-300 ease-in-out shadow-md origin-center hover:scale-105 hover:shadow-lg hover:shadow-[#e8a020]/40`}>
      {text}
     </button></Link>
   )
